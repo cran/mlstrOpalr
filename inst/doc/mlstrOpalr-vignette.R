aac073194762c203ca2a92621b17d1a7d0188e1a
Guillaume Fabre <guijoseph.fabre@gmail.com>
2023-06-23 18:20:02 +0000

    version 1.0.0

---
+++ inst/doc/mlstrOpalr-vignette.R
@@ -0,0 +1,18 @@
+## ---- include = FALSE---------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ---- eval=FALSE--------------------------------------------------------------
+#  # To update the R package in your R environment you may first need to remove
+#  # it, and use the exit command quit() to terminate the current R session.
+#  
+#  # To install the R package:
+#  install.packages('mlstrOpalr')
+#  library(mlstrOpalr)
+#  
+#  #if you need help with the package, please use:
+#  mlstrOpalr_help()
+#  
+
